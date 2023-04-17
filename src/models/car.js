038b8b0e1c5018dc70513a44e379232a1646fbd9
Chris Wood <wchristopherwood912@gmail.com>
2023-04-17 14:58:43 -0700

    cars schema created

---
+++ src/models/car.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+// Add your models here.
+const CarSchema = new Schema({
+    make: {type: String, required: true},
+    model: {type: String, required: true},
+    model_years: {type: Number, required: true},
+    number_of_doors: {type: Number, required: true},
+    modern_hp: {type: Number, required: true}
+})
+
+const Car = mongoose.model('Car', CarSchema)
+
+model.exports = Car
