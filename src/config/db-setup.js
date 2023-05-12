7685c8940c0d1ac00e0469d4880629a0fc8f9d39
Chris Wood <wchristopherwood912@gmail.com>
2023-05-12 16:48:35 -0400

    sends data to right database now

--- src/config/db-setup.js
+++ src/config/db-setup.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 // connect to mongo db
-const mongoUri = process.env.MONGODB_URI
-mongoose.connect(mongoUri)
+const mongoUri = process.env.MONGODB_URI;
+mongoose.connect(mongoUri);
 
 mongoose.connection.on('error', () => {
-  throw new Error(`unable to connect to database: ${mongoUri}`)
-})
+  throw new Error(`unable to connect to database: ${mongoUri}`);
+});
 
-module.exports = mongoose.connection
+module.exports = mongoose.connection;
